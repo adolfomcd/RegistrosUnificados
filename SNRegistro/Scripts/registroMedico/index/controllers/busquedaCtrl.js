e94fbc735e11bd18efe988f1077ad37e0eccc957
Nestornjrz <nestornjrz@hotmail.com>
2015-06-17 16:11:01 -0400

    Preparacion del FrontEnd para la busqueda por ciudadano

---
+++ SNRegistro/Scripts/registroMedico/index/controllers/busquedaCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('GestorMedicoApp')
+        .controller('busquedaCtrl', busquedaCtrl);
+
+    busquedaCtrl.$inject = ['GestorMedicoResourse'];
+
+    function busquedaCtrl(GestorMedicoResourse) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.GestorMedico = {};
+        vm.Ciudadanos = GestorMedicoResourse.Ciudadanos.query();
+    }
+})();
